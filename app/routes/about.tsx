1333900c95fa2b4577a252a853475d548be7f53d
M Haidar Hanif <git@mhaidarhanif.com>
2024-10-20 15:20:58 +0700

    feat: about form example

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,6 +1,9 @@
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
+
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 
 export function loader() {
   const teamMembers = [{ name: "A" }, { name: "B" }];
@@ -8,11 +11,24 @@ export function loader() {
   return json({ teamMembers });
 }
 
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+
+  const teamMember = {
+    name: String(formData.get("name")),
+  };
+
+  console.log({ teamMember });
+
+  return json({ teamMember });
+}
+
 export default function About() {
   const { teamMembers } = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
 
   return (
-    <div>
+    <div className="container px-4">
       <h1>About</h1>
       <p>This is the about page.</p>
       <Button asChild>
@@ -24,6 +40,14 @@ export default function About() {
           return <li key={teamMembers.name}>{teamMembers.name}</li>;
         })}
       </ul>
+
+      <Form method="post">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" name="name" defaultValue="Siapa" />
+        <Button type="submit">Submit</Button>
+      </Form>
+
+      <pre>{JSON.stringify(actionData, null, 2)}</pre>
     </div>
   );
 }
