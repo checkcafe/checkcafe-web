90980563aba07684b33659fc79b1b7b61df18a3c
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-10-22 23:37:42 +0700

    feat: add input email in home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,6 +4,7 @@ import type { MetaFunction } from "@remix-run/node";
 import HeroSection from "~/components/shared/hero-section";
 import HomePopularPlaces from "~/components/shared/home-popular-places/home-popular-places";
 import HomeExploreCity from "~/components/shared/home-explore-city/home-explore-city";
+import HomeInputEmail from "~/components/shared/home-input-email";
 
 export const meta: MetaFunction = () => {
   return [
@@ -22,6 +23,7 @@ export default function Index(): React.ReactElement {
       <HeroSection />
       <HomePopularPlaces />
       <HomeExploreCity />
+      <HomeInputEmail />
     </div>
   );
 }
