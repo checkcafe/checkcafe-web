687817dc11bdf1c52233f84eede4b1e1956b22d6
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2024-10-22 17:43:22 +0700

    feat:add hero section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,27 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Button } from "~/components/ui/button";
+import type { MetaFunction } from "@remix-run/node"
+import HeroSection from "~/components/shared/hero-section"
+import { Button } from "~/components/ui/button"
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "CheckCafe" },
-    {
-      name: "description",
-      content:
-        "Check the best cafe for social, food, WFC, and comfortable experience",
-    },
-  ];
-};
+    return [
+        { title: "CheckCafe" },
+        {
+            name: "description",
+            content: "Check the best cafe for social, food, WFC, and comfortable experience",
+        },
+    ]
+}
 
 export default function Index() {
-  return (
-    <div className="flex justify-center">
-      <header className="space-y-4 p-4">
-        <h1 className="text-2xl font-bold">☕CheckCafe</h1>
-        <h2 className="text-xl">
-          Check the best cafe for social, food, WFC, and comfortable experience
-        </h2>
-        <Button>Coming Soon</Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flex flex-col justify-center">
+            <HeroSection />
+        </div>
+    )
 }
