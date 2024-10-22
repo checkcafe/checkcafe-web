ba77a7b6a9df375379eb942fdea0b48e88c76b9c
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2024-10-22 09:43:26 +0700

    feat:add mapbox view

--- app/components/shared/navbar.tsx
+++ app/components/shared/navbar.tsx
@@ -1,40 +1,34 @@
-import { Link } from "@remix-run/react";
-import { ProfileIcon } from "../icons/icons";
-import { Input } from "~/components/ui/input";
+import { Link } from "@remix-run/react"
+import { ProfileIcon } from "../icons/icons"
+import { Input } from "~/components/ui/input"
 
 export function Navbar() {
-  return (
-    <nav className='  fixed top-0  w-full flex m-0 justify-between   p-8 bg-[#F6EBDA] '>
-      <div className='md:flex flex-col gap-4'>
-        <Link to={"/"}>
-          <h2 className='text-3xl font-jacques  tracking-tight text-gray-900'>
-            ☕CheckCafe
-          </h2>
-        </Link>
-      </div>
+    return (
+        <nav className="  fixed top-0  w-full flex m-0 justify-between z-50  p-8 bg-[#F6EBDA] ">
+            <div className="md:flex flex-col gap-4">
+                <Link to={"/"}>
+                    <h2 className="text-3xl font-jacques  tracking-tight text-gray-900">☕CheckCafe</h2>
+                </Link>
+            </div>
 
-      <Input
-        type='search'
-        placeholder='Search Cafes'
-        className='w-4/12 shadow-lg'
-      />
-      <div className='mt-12 md:mt-0 flex gap-4'>
-        <ul className='flex gap-8 font-semibold text-base self-center '>
-          <li>
-            <Link to={"/about"} className='text-primButton'>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to={"/favourite"} className='text-primButton'>
-              Favourite
-            </Link>
-          </li>
-        </ul>
-        <Link to={"/profile"} className='text-primButton self-center'>
-          <ProfileIcon className='w-10 h-10' />
-        </Link>
-      </div>
-    </nav>
-  );
+            <Input type="search" placeholder="Search Cafes" className="w-4/12 shadow-lg" />
+            <div className="mt-12 md:mt-0 flex gap-4">
+                <ul className="flex gap-8 font-semibold text-base self-center ">
+                    <li>
+                        <Link to={"/about"} className="text-primButton">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={"/favourite"} className="text-primButton">
+                            Favourite
+                        </Link>
+                    </li>
+                </ul>
+                <Link to={"/profile"} className="text-primButton self-center">
+                    <ProfileIcon className="w-10 h-10" />
+                </Link>
+            </div>
+        </nav>
+    )
 }
---
+++ app/components/ui/mapbox-view.tsx
@@ -0,0 +1,148 @@
+import { useRef } from "react"
+import { useNavigate } from "react-router-dom"
+import { Map, Source, Layer, type MapRef, type GeoJSONSource } from "react-map-gl"
+import { type GeoJSONFeature } from "mapbox-gl"
+import { type FeatureCollection } from "geojson"
+
+import "mapbox-gl/dist/mapbox-gl.css"
+import { clusterCountLayer, clusterLayer, unclusteredPointLayer } from "~/configs/layer"
+
+const MAPBOX_TOKEN =
+    "pk.eyJ1IjoiaWttYWw5NiIsImEiOiJjbTJqN3BpcGgwMnU3MmpvcW96NTRtb2RoIn0.jxkI7uKnc3mvMqWgIMKuhg"
+
+type FeatureProperties = {
+    type: "Place"
+    id: string
+    title: string
+    slug: string
+}
+
+export function MapboxView({
+    places,
+    initialViewState = {
+        latitude: -0.4752106,
+        longitude: 116.6995672,
+        zoom: 4,
+    },
+    onPlaceClick,
+}: {
+    places: any
+    initialViewState?: {
+        latitude: number
+        longitude: number
+        zoom: number
+    }
+    onPlaceClick: (placeId: string) => void
+}) {
+    const navigate = useNavigate()
+    const mapRef = useRef<MapRef>(null)
+
+    const geojson: FeatureCollection = {
+        type: "FeatureCollection",
+        features: places.map((place: any) => ({
+            type: "Feature",
+            properties: {
+                type: "Place",
+                id: place.id,
+                name: place.name,
+                active: false,
+            },
+            geometry: {
+                type: "Point",
+                coordinates: [place.longitude, place.latitude],
+            },
+        })),
+    }
+
+    const onClick = (event: any) => {
+        const features = event.features as GeoJSONFeature[]
+        if (!features.length) return
+
+        const feature = features[0]
+        const featureProperties = feature.properties as FeatureProperties
+
+        if (featureProperties.type === "Place") {
+            const placeId = featureProperties.id
+            onPlaceClick(placeId)
+
+            // Get the coordinates of the clicked point
+            if (feature.geometry.type === "Point") {
+                const [longitude, latitude] = feature.geometry.coordinates
+
+                // Zoom in to the clicked point
+                mapRef.current?.flyTo({
+                    center: [longitude, latitude],
+                    zoom: 12, // Set your desired zoom level here
+                    speed: 2, // Set the speed of the zoom transition
+                    curve: 1, // Set the curve of the zoom transition
+                    easing(t) {
+                        return t // You can customize the easing function here
+                    },
+                })
+            }
+
+            const updatedGeojson = {
+                ...geojson,
+                features: geojson.features.map((feat) => {
+                    if (!feat.properties) {
+                        return feat
+                    }
+
+                    return {
+                        ...feat,
+                        properties: {
+                            ...feat.properties,
+                            active: feat.properties.id === placeId,
+                        },
+                    }
+                }),
+            }
+
+            ;(mapRef.current?.getSource("places-source") as GeoJSONSource)?.setData(updatedGeojson)
+        }
+    }
+
+    const onMouseEnter = (event: any) => {
+        const features = event.features as GeoJSONFeature[]
+        if (features.length) {
+            const canvas = mapRef.current?.getCanvas()
+            if (canvas) {
+                canvas.style.cursor = "pointer"
+            }
+        }
+    }
+
+    const onMouseLeave = () => {
+        const canvas = mapRef.current?.getCanvas()
+        if (canvas) {
+            canvas.style.cursor = ""
+        }
+    }
+
+    return (
+        <Map
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            style={{ width: "100%", height: "100vh", borderRadius: 5 }}
+            initialViewState={initialViewState}
+            mapboxAccessToken={MAPBOX_TOKEN}
+            interactiveLayerIds={[String(clusterLayer.id), String(unclusteredPointLayer.id)]}
+            onClick={onClick}
+            onMouseMove={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            ref={mapRef}
+        >
+            <Source
+                id="places-source"
+                type="geojson"
+                data={geojson}
+                cluster={true}
+                clusterMaxZoom={14}
+                clusterRadius={50}
+            >
+                <Layer {...clusterLayer} />
+                <Layer {...clusterCountLayer} />
+                <Layer {...unclusteredPointLayer} />
+            </Source>
+        </Map>
+    )
+}
