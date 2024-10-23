9f5367ce2316404d1fa61a798ccc4b1a266b4b7f
naswa <endabelyuproject@gmail.com>
2024-10-23 21:32:22 +0700

    feat(icons): add new icons magnify glasses,

--- app/root.tsx
+++ app/root.tsx
@@ -2,11 +2,12 @@ import {
   Links,
   Meta,
   Outlet,
+  redirect,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-
+import React from "react";
 import "./tailwind.css";
 import { Footer } from "./components/shared/footer";
 import { Navbar } from "./components/shared/navbar";
@@ -31,7 +32,14 @@ export const links: LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
+import { ActionFunctionArgs } from "@remix-run/node";
 
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const searchQuery = formData.get("search");
+
+  return redirect(`/places?query=${encodeURIComponent(searchQuery as string)}`);
+}
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
