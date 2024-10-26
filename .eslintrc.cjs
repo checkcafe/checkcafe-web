bf98d747676bdae9c8347921628eb3f0567c9359
M Haidar Hanif <git@mhaidarhanif.com>
2024-10-26 23:49:36 +0700

    fix: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,3 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
@@ -35,6 +29,9 @@ module.exports = {
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
       ],
+      rules: {
+        "react/prop-types": 0,
+      },
       settings: {
         react: {
           version: "detect",
@@ -68,8 +65,8 @@ module.exports = {
       },
       extends: [
         "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
         "plugin:import/typescript",
+        // "plugin:import/recommended",
       ],
     },
 
