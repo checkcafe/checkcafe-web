0c0ccba5c5a36d25fb2ff92e46874f3e1908ef30
M Haidar Hanif <git@mhaidarhanif.com>
2024-10-26 21:36:32 +0700

    fix: home and about

--- app/components/shared/navbar.tsx
+++ app/components/shared/navbar.tsx
@@ -7,17 +7,19 @@ import { ProfileIcon } from "../icons/icons";
 
 export function Navbar({ cookie }: { cookie: string }) {
   return (
-    <nav className=" sticky top-0  w-full flex m-0 justify-between p-8 z-50 bg-background ">
+    <nav className="sticky top-0 w-full flex m-0 justify-between p-8 z-50 bg-amber-50">
       <div className="md:flex flex-col gap-4">
         <Link to={"/"}>
-          <h2 className="text-3xl font-jacques  tracking-tight text-gray-900">
+          <h2 className="text-3xl font-brand tracking-tight text-gray-900">
             ☕CheckCafe
           </h2>
         </Link>
       </div>
+
       <Searchbar />
+
       <div className="mt-12 md:mt-0 flex gap-4">
-        <ul className="flex gap-8 font-semibold text-base self-center ">
+        <ul className="flex items-center gap-8 font-semibold text-base self-center ">
           <li>
             <Link to={"/places"} className="text-primary">
               Places
@@ -28,18 +30,21 @@ export function Navbar({ cookie }: { cookie: string }) {
               About
             </Link>
           </li>
+          <li>
+            {!cookie && (
+              <Button asChild>
+                <Link to={"/login"} className="text-primary self-center">
+                  Login
+                </Link>
+              </Button>
+            )}
+            {cookie && (
+              <Link to={"/profile"} className="text-primary self-center">
+                <ProfileIcon className="w-10 h-10" />
+              </Link>
+            )}
+          </li>
         </ul>
-        {cookie ? (
-          <Link to={"/profile"} className="text-primary self-center">
-            <ProfileIcon className="w-10 h-10" />
-          </Link>
-        ) : (
-          <Button asChild>
-            <Link to={"/login"} className="text-primary self-center">
-              Login
-            </Link>
-          </Button>
-        )}
       </div>
     </nav>
   );
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,9 +1,5 @@
-import { ActionFunctionArgs, json } from "@remix-run/node";
-import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
-
-import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export function loader() {
   const teamMembers = [{ name: "A" }, { name: "B" }];
@@ -11,41 +7,13 @@ export function loader() {
   return json({ teamMembers });
 }
 
-export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-
-  const teamMember = {
-    name: String(formData.get("name")),
-  };
-
-  return json({ teamMember });
-}
-
 export default function About() {
   const { teamMembers } = useLoaderData<typeof loader>();
-  const actionData = useActionData<typeof action>();
 
   return (
     <div className="container px-4">
       <h1>About</h1>
-      <p>This is the about page.</p>
-      <Button asChild>
-        <Link to="/">Home</Link>
-      </Button>
-
-      <ul>
-        {teamMembers.map((teamMembers) => {
-          return <li key={teamMembers.name}>{teamMembers.name}</li>;
-        })}
-      </ul>
-
-      <Form method="post">
-        <Label htmlFor="name">Name</Label>
-        <Input type="text" name="name" defaultValue="Siapa" />
-        <Button type="submit">Submit</Button>
-      </Form>
-
-      <pre>{JSON.stringify(actionData, null, 2)}</pre>
+      <pre>{JSON.stringify(teamMembers, null, 2)}</pre>
     </div>
   );
 }
