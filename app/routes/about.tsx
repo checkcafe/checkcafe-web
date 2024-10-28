73b9d1fdde680274d89428a646928fb15ce3c1cd
Zacky Achmad <zckyachmd@gmail.com>
2024-10-28 07:26:23 +0700

    feat: improve avatar size for mobile view

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,38 +1,44 @@
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 
 export function loader() {
   const teamMembers = [
-    { 
-      name: "Zaky",
-      image: '/assets/img/zaky.png',
-      linkedIn: 'https://www.linkedin.com/in/zckyachmd'
-    }, 
-    { 
-      name: "Aan",
-      image: '/assets/img/aan.png',
-      linkedIn: 'https://www.linkedin.com/in/muhammad-farhan-569018185'
-    }, 
-    { 
-      name: "Naswa",
-      image: '/assets/img/naswa.jpg',
-      linkedIn: 'https://www.linkedin.com/in/naswa-wilantama'
-    }, 
-    { 
-      name: "Iqbal",
-      image: '/assets/img/iqbal.jpg',
-      linkedIn: 'https://www.linkedin.com/in/iqbal-chaidir-8b80a4251/' 
-    }, 
-    { 
-      name: "Difa",
-      image: '/assets/img/difa.png',
-      linkedIn: 'https://www.linkedin.com/in/difasulthon' 
-    }, 
-    { 
+    {
       name: "Haidar",
-      image: '/assets/img/haidar.jpg',
-      linkedIn: '' 
-    }
+      image: "/assets/img/haidar.jpg",
+      role: "Project Manager",
+      linkedIn: "https://www.linkedin.com/in/mhaidarhanif/",
+    },
+    {
+      name: "Zaky",
+      image: "/assets/img/zaky.png",
+      role: "Tech Lead & Backend Developer",
+      linkedIn: "https://www.linkedin.com/in/zckyachmd",
+    },
+    {
+      name: "Aan",
+      image: "/assets/img/aan.png",
+      role: "Backend Developer",
+      linkedIn: "https://www.linkedin.com/in/muhammad-farhan-569018185",
+    },
+    {
+      name: "Naswa",
+      image: "/assets/img/naswa.jpg",
+      role: "Frontend Developer",
+      linkedIn: "https://www.linkedin.com/in/naswa-wilantama",
+    },
+    {
+      name: "Iqbal",
+      image: "/assets/img/iqbal.jpg",
+      role: "Frontend Developer",
+      linkedIn: "https://www.linkedin.com/in/iqbal-chaidir-8b80a4251/",
+    },
+    {
+      name: "Difa",
+      image: "/assets/img/difa.png",
+      role: "Frontend Developer & UI/UX Designer",
+      linkedIn: "https://www.linkedin.com/in/difasulthon",
+    },
   ];
 
   return json({ teamMembers });
@@ -43,25 +49,52 @@ export default function About() {
 
   return (
     <div className="w-full">
-      <section className="flex h-44 justify-center items-center bg-amber-50">
-        <section className="flex flex-row gap-3">
-          <h1 className="text-5xl font-medium text-amber-900">Want to know </h1>
-          <h1 className="text-5xl font-medium text-amber-500"> about</h1>
-          <h1 className="text-5xl font-medium text-amber-900"> us?</h1>
+      <section className="flex h-44 items-center justify-center bg-amber-50 px-4 sm:px-6">
+        <section className="flex flex-wrap gap-1 sm:gap-3">
+          <h1 className="text-3xl font-medium text-amber-900 sm:text-4xl lg:text-5xl">
+            Want to know
+          </h1>
+          <h1 className="text-3xl font-medium text-amber-500 sm:text-4xl lg:text-5xl">
+            about
+          </h1>
+          <h1 className="text-3xl font-medium text-amber-900 sm:text-4xl lg:text-5xl">
+            us?
+          </h1>
         </section>
       </section>
-      <section className="flex flex-col px-32 mt-6 items-center justify-center">
-        <section className="flex flex-col justify-center items-center gap-3">
-          <h1 className="text-5xl font-medium text-amber-900">Our Team</h1>
-          <h1 className="text-xl font-normal text-amber-900">Get to know the passionate professionals behind our products and services</h1>
+
+      <section className="mt-6 flex flex-col items-center justify-center px-4 sm:px-8 md:px-16 lg:px-32">
+        <section className="flex flex-col items-center justify-center gap-2 text-center sm:gap-3">
+          <h1 className="text-3xl font-medium text-amber-900 sm:text-4xl lg:text-5xl">
+            Our Team
+          </h1>
+          <p className="text-lg font-normal text-amber-900 sm:text-xl lg:text-2xl">
+            Get to know the passionate professionals behind our products and
+            services
+          </p>
         </section>
-        <div className="mt-12 grid grid-cols-3 justify-items-center w-2/4">
-          {teamMembers.map(member => (
-            <a href={member.linkedIn} target="_blank" rel="noopener noreferrer">
-              <div className="fle- flex-col items-center mb-10 hover:opacity-50 hover:cursor-pointer">
-                <img src={member.image} alt={member.name} className="h-32 w-32 rounded-full border-4 bg-white border-amber-900 shadow-xl">
-                </img>
-                <p className="text-lg font-semibold text-amber-900 mt-4 text-center">{member.name}</p>
+
+        <div className="w-full/2 mt-8 grid grid-cols-1 justify-items-center gap-x-8 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-10 lg:gap-y-6">
+          {teamMembers.map((member, index) => (
+            <a
+              key={index}
+              href={member.linkedIn}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-3/4 sm:w-full"
+            >
+              <div className="flex flex-col items-center hover:cursor-pointer hover:opacity-50">
+                <img
+                  src={member.image}
+                  alt={member.name}
+                  className="h-24 w-24 rounded-full border-4 border-amber-900 bg-white object-cover shadow-xl sm:h-28 sm:w-28 md:h-32 md:w-32 lg:h-36 lg:w-36"
+                />
+                <p className="sm:text-md mt-1 text-center text-sm font-semibold text-amber-900 md:text-lg">
+                  {member.name}
+                </p>
+                <p className="text-center text-xs text-gray-600 sm:text-sm">
+                  {member.role}
+                </p>
               </div>
             </a>
           ))}
